6262599c968f856eca1798c8592825e5f682b919
LeitoBarrera <barrerajaider22@gmail.com>
2025-04-25 19:56:26 -0500

    Instalar laravel y configuración basica

---
+++ app/Http/Controllers/CoctelController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\facades\Request;
+
+class CoctelController extends Controller
+{
+    public function index()
+    {
+        
+    }
+}
